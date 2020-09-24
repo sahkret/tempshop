c9f539c32ab9ea589da2b33241923971aadd715f
Alex Petersen <alex806a@edu.easj.dk>
2020-09-24 02:47:08 +0200

    Dropdown ditched, header finito - Creating footer, then styling products OR copy-pasta'ing php around for full site functionality

--- wordpress/wp-content/themes/tempshop-theme/header.php
+++ wordpress/wp-content/themes/tempshop-theme/header.php
@@ -26,19 +26,25 @@
                 <a href="<?php bloginfo('url');?>">
                     <img class="img-fluid logo" src="<?php echo $logo[0] ?>" alt="logo">
                 </a>
-
-                <?php
-                    wp_nav_menu(
-                        array(
-                            'menu' => 'primary',
-                            'container' => '',
-                            'theme_location' => 'primary',
-                            //Insert ul classes for nav here - li classes go through dashboard->menus->css class
-                            'items_wrap' => '<ul id="" class="menu menu__header d-flex">%3$s</ul>'
-                            //a tags get no class, edit bootstrap css to .nav-item a - Icon markup goes straight into wp nav labels
-                        )
-                    )
-                ?>
+                <nav class="navbar navbar-expand-lg flex-md-column flex-lg-row">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+                    </button>
+                    <article class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <?php
+                            wp_nav_menu(
+                                array(
+                                    'menu' => 'primary',
+                                    'container' => '',
+                                    'theme_location' => 'primary',
+                                    //Insert ul classes for nav here - li classes go through dashboard->menus->css class
+                                    'items_wrap' => '<ul id="" class="menu menu__header d-flex navbar flex-sm-column flex-lg-row">%3$s</ul>'
+                                    //a tags get no class, edit bootstrap css to .nav-item a - Icon markup goes straight into wp nav labels
+                                )
+                            )
+                        ?>
+                    </article>
+                </nav>
             </article>
         </div>
     </section>
